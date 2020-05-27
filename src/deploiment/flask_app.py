6aa22a0de96d3ec55690ed549e18f380e5263c2c
AKRICHE Lakri <akriche.lakri@gmail.com>
2020-05-27 12:09:45 +0200

    updates

---
+++ src/deploiment/flask_app.py
@@ -0,0 +1,46 @@
+
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('../machine_learning/model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predictions.html')
+def predictions():
+    return render_template('predictions.html')
+
+@app.route('/actualities.html')
+def actualites():
+    return render_template('actualities.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='The prices will propably be as follow $ {}'.format(output))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ src/scrapping/Custom_dataset.py
@@ -0,0 +1,5 @@
+class Dataset():
+    def __init__(self, company, start_date, end_date):
+        self.company = company
+        self.start_date = start_date
+        self.end_date = end_date
