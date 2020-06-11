71a71bd207bc4061ed1b381e57df8efe0ac033a7
AKRICHE Lakri <akriche.lakri@gmail.com>
2020-06-11 17:27:53 +0200

    data preprocessing: work in progress

--- src/scrapping/preprocessing.py
+++ src/scrapping/preprocessing.py
@@ -17,9 +17,49 @@ class Custom_dataset():
         self.prices = pd.read_csv(self.prices_path)
 
 
+    def clean_data(self):
+        self.get_data()
+        #print(self.prices.shape)
+        #print(self.prices[self.prices.duplicated()])
+        #group tweets of the same day since our stock prices ar daily
+        #self.clean_tweets()
+        self.clean_prices()
+
+
+
+    def clean_tweets(self):
+
+        self.tweets["date"] = pd.to_datetime(self.tweets["date"], format='%Y/%m/%d')
+        self.tweets["date"] = self.tweets["date"].dt.date
+        self.tweets['expand'] = self.tweets.apply(lambda x: ', '.join([x['text']]), axis=1)
+        self.tweets = self.tweets.groupby('date')['expand'].apply(list)
+        self.tweets = pd.DataFrame(data=self.tweets.values, index=self.tweets.index, columns=["text"])
+        self.tweets.to_csv("data/clean_tweets.csv")
+
+    def clean_prices(self):
+        #drop duplicate
+        self.prices = self.prices.drop_duplicates()
+        #delete zeros columns
+        self.prices = self.prices.loc[:, (self.prices != 0).any(axis=0)]
+
+        self.prices["Date"] = pd.to_datetime(self.prices["Date"], format='%Y/%m/%d')
+        self.prices["Date"] = self.prices["Date"].dt.date
+
+
 if __name__ == "__main__":
     df = Custom_dataset()
-    df.get_data()
-    print(df.tweets.head())
+    df.clean_data()
+    print(df.prices)
+
+    """"  t = df.tweets
+    t["date"] = pd.to_datetime(t["date"], format='%Y/%m/%d')
+
+    t["date"]=t["date"].dt.date
+    t['expand'] = t.apply(lambda x: ', '.join([x['text']]), axis=1)
+    t=t.groupby('date')['expand'].apply(list)
+    t = pd.DataFrame(data=t.values, index=t.index, columns=["text"])
+    print(t.head())
+    """
+
 
 
